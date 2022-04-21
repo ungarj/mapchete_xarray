aea57842e15f91018bccfc32cf656d9e02f256f5
Elias Wanko <elias.wanko@eox.at>
2022-04-21 12:02:28 +0200

    write initial arrays with zarr instead of xarray/dask

--- mapchete_xarray/_zarr.py
+++ mapchete_xarray/_zarr.py
@@ -1,7 +1,3 @@
-import dask.array as da
-import json
-from mapchete.io import fs_from_path
-import os
 from tilematrix._funcs import Bounds
 import xarray as xr
 from zarr.storage import FSStore
@@ -24,7 +20,6 @@ def initialize_zarr(
     y_axis_name="Y",
     area_or_point="Area",
 ):
-    fs = fs_from_path(path)
 
     if time:
         start_time = (
@@ -61,12 +56,7 @@ def initialize_zarr(
 
     coord_x = [bounds.left + pixel_x_size / 2 + i * pixel_x_size for i in range(width)]
     coord_y = [bounds.top + pixel_y_size / 2 + i * pixel_y_size for i in range(height)]
-    array = da.full(
-        shape=output_shape,
-        fill_value=fill_value,
-        chunks=output_chunks,
-        dtype=dtype,
-    )
+
     coords = {
         x_axis_name: ([x_axis_name], coord_x),
         y_axis_name: ([y_axis_name], coord_y),
@@ -76,21 +66,11 @@ def initialize_zarr(
         ["time", y_axis_name, x_axis_name] if time else [y_axis_name, x_axis_name]
     )
 
-    data_vars = {f"Band{i}": ([*axis_names], array) for i in range(1, count + 1)}
-
     if time:
         coords["time"] = coord_time
 
-    ds = xr.Dataset(coords=coords, data_vars=data_vars)
+    ds = xr.Dataset(coords=coords)
 
-    attrs = {
-        "_ARRAY_DIMENSIONS": axis_names,
-        # xarray cannot write attributes values as dictionaries!
-        # "_CRS": {"wkt": crs.wkt},
-        "AREA_OR_POINT": area_or_point,
-    }
-    for data_var in ds.data_vars:
-        ds[data_var].attrs = attrs
     ds.to_zarr(
         FSStore(path),
         compute=False,
@@ -98,14 +78,19 @@ def initialize_zarr(
         safe_chunks=True,
     )
 
-    # TODO: find a better way!
-    for data_var in ds.data_vars:
-        attrs_path = os.path.join(path, data_var, ".zattrs")
-        with fs.open(attrs_path, mode="r") as src:
-            metadata = json.loads(src.read())
-        metadata["_CRS"] = {"wkt": crs.wkt}
-        with fs.open(attrs_path, mode="w") as dst:
-            dst.write(json.dumps(metadata))
+    for i in range(1, count + 1):
+        store = FSStore(f"{path}/Band{i}")
+        zarr.creation.create(
+            shape=output_shape,
+            chunks=output_chunks,
+            dtype=dtype,
+            store=store,
+        )
+
+        attrs = zarr.open(store).attrs
+        attrs["_ARRAY_DIMENSIONS"] = axis_names
+        attrs["_CRS"] = {"wkt": crs.wkt}
+        attrs["AREA_OR_POINT"] = area_or_point
 
     zarr.consolidate_metadata(path)
 
